31590242debf703ad8658bb56256927b05c68297
nporter <nporterwork@gmail.com>
2023-05-03 19:20:59 -0700

    changes to ids and bug fixes

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,6 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  // target the savebutton give it an onclick event listener
-
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+ 
   function createTimeBlocks() {
-      const currentHour = dayjs().hour();
       // run through the iteration of 9-5 on a 24 hour clock
       for (let hour = 9; hour <= 17; hour++) {
           // if over 12 add pm if =12 add pm else add am
@@ -77,19 +60,11 @@ $(function () {
       localStorage.setItem(timeBlockId, JSON.stringify(eventData));
   };
 
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
   // Gets the specified hour from local storage
   function getSavedTimeBlock(hour) {
       const savedItem = localStorage.getItem(hour);
       return savedItem ? JSON.parse(savedItem).description : '';
   }
-
-
-  // TODO: Add code to display the current date in the header of the page.
   // Sets the current date 
   function setCurrentDay() {
       // Get the current date using Day.js
